fc0a7f6ed913f846f23f50dab333cd075143541a
br00tm <pedrolbmoreira23@gmail.com>
2025-03-28 09:21:34 -0300

    modified:   .gitignore 	new file:   src/models/Avaliacao.js 	modified:   src/models/Evento.js 	new file:   src/models/Palestrante.js

---
+++ src/models/Evento.js
@@ -0,0 +1,32 @@
+import { Model, DataTypes } from 'sequelize';
+
+class Evento extends Model {
+  static init(sequelize) {
+    super.init({
+      nome: { 
+        type: DataTypes.STRING, 
+        validate: {
+          notEmpty: { msg: "Nome do Evento deve ser preenchido!" },
+          len: { args: [2, 50], msg: "Nome do Evento deve ter entre 2 e 50 letras!" }
+        }
+      },
+      data: { 
+        type: DataTypes.DATEONLY, 
+        validate: {
+          isDate: { msg: "Data do Evento deve ser preenchida!" },
+          is: {args: ["[0-9]{4}\-[0-9]{2}\-[0-9]{2}"], msg: "Data do Evento deve seguir o padr�o yyyy-MM-dd!" }
+        }
+      }
+    }, { sequelize, modelName: 'evento', tableName: 'eventos' })
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Local, { foreignKey: 'local_id', as: 'local' });
+    this.belongsTo(models.Avaliacao, { foreignKey: 'avaliacao_id', as: 'avaliacao' });
+    this.belongsToMany(models.Palestrante, { through: 'evento_palestrante', as: 'palestrantes' });
+    this.belongsToMany(models.Patrocinador, { through: 'evento_patrocinador', as: 'patrocinadores' });
+    this.belongsToMany(models.Funcionario, { through: 'evento_funcionario', as: 'funcionarios' });
+  }
+}
+
+export { Evento };
