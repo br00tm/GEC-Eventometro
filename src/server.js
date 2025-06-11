f1719859cd40c93c8b9455ec019e5a237eb3d0a3
Pedro Brito <pedrolbmoreira23@gmail.com>
2025-06-11 09:35:24 -0300

    modified:   src/server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 import express from "express";
 import routes from './routes.js';
 import errorHandler from '../src/_middleware/error-handler.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Para obter o __dirname no ES Module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Importando configuração e estabelecimento da conexão com o banco de dados
 import sequelize from './config/database-connection.js';
@@ -32,6 +38,9 @@ app.use(function (req, res, next) {
 // Limite máximo de request aumentado
 app.use(express.json({limit: '10mb'}));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
+// Configuração para servir arquivos estáticos da pasta public
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use(routes);
 app.use(errorHandler); // Manipulador de erro global (error handler)
